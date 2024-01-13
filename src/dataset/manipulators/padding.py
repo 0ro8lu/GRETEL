7efce862704a02907c1bd1c1dc761e442806c8a0
Giacomo <frascarelli.1917888@studenti.uniroma1.it>
2024-01-13 19:30:52 +0100

    fixed bug in padding

--- src/dataset/manipulators/padding.py
+++ src/dataset/manipulators/padding.py
@@ -12,5 +12,5 @@ class GraphPadding(BaseManipulator):
         n_nodes=instance.data.shape[0]
         num_padding= max(0,max_nodes-n_nodes)
         instance.data=np.pad(instance.data,((0,num_padding),(0,num_padding)),'constant',constant_values=0)
-        instance.node_features=np.pad(instance.node_features,((0,num_padding),(0,0)),'constant',constant_values=2)
+        instance.node_features=np.pad(instance.node_features,((0,num_padding),(0,0)),'constant',constant_values=0)
         return {}
